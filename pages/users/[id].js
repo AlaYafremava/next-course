f5311207cb6a51fe249bbb294d87ca9e94e75378
Ala <yefremova.alla@gmail.com>
2024-08-16 14:35:59 +0300

    fix: name of scss file

--- pages/users/[id].js
+++ pages/users/[id].js
@@ -1,5 +1,5 @@
 import { useRouter } from "next/router";
-import styles from "../../styles/user.module.scss";
+import styles from "../../styles/User.module.scss";
 
 export default function User() {
   const { query } = useRouter();
